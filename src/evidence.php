79430959b6987a17307612e28130ec28e867c81c
Vítězslav Dvořák <vitex@arachne.cz>
2016-09-30 15:24:15 +0200

    Discover all relations Do not crash on unexistent evidence listig

--- src/evidence.php
+++ src/evidence.php
@@ -32,94 +32,119 @@ if (!is_null($action)) {
     $oPage->redirect('query.php?evidence='.$evidence.'&action='.$action.'&id='.$id);
 }
 
-$oPage->addItem(new ui\PageTop(sprintf(_('Evidence %s'),
-        \FlexiPeeHP\EvidenceList::$name[$evidence])));
+if (!isset(\FlexiPeeHP\EvidenceList::$name[$evidence])) {
+    $oPage->addStatusMessage(sprintf(_('Evidence %s does not exist'), $evidence),
+        'warning');
+    $oPage->redirect('evidences.php');
+} else {
+
+
+    $oPage->addItem(new ui\PageTop(sprintf(_('Evidence %s'),
+            \FlexiPeeHP\EvidenceList::$name[$evidence])));
 
 //$oPage->container->addItem(new );
 
-$evobj = new Flexplorer($evidence);
+    $evobj = new Flexplorer($evidence);
 
-$tabs = new \Ease\TWB\Tabs('EviTabs');
-$tabs->addTab(_('Listing'),
-    new ui\DataGrid(_('Evidence'), new DataSource($evobj)));
-$tabs->addTab(_('Structure'), new ui\EvidenceProperties($evobj, $column),
-    isset($column));
+    $tabs = new \Ease\TWB\Tabs('EviTabs');
+    $tabs->addTab(_('Listing'),
+        new ui\DataGrid(_('Evidence'), new DataSource($evobj)));
+    $tabs->addTab(_('Structure'), new ui\EvidenceProperties($evobj, $column),
+        isset($column));
 
-$method = $oPage->getRequestValue('method');
-$body   = $oPage->getRequestValue('body');
-if (is_null($body)) {
-    $body = $evobj->jsonizeData([]);
-}
-
-$tabs->addTab(_('Query'),
-    new \Ease\TWB\Panel(_('User Query'), 'warning',
-    new ui\SendForm($url, $method, $body)));
-
-$overviewUrl = $evobj->getEvidenceUrl().'/properties.html?inDesktopApp=true';
-
-$tabs->addTab(_('Items overview'),
-    new \Ease\Html\IframeTag($overviewUrl,
-    ['style' => 'width: 100%; height: 600px', 'frameborder' => 0]));
-if (strstr($url, '?')) {
-    $overviewUrl = $url.'&inDesktopApp=true';
-} else {
-    $overviewUrl = $url.'?inDesktopApp=true';
-}
-$tabs->addTab(_('FlexiBee'),
-    new \Ease\Html\IframeTag(str_replace('.json', '.html', $overviewUrl),
-    ['style' => 'width: 100%; height: 600px', 'frameborder' => 0]));
-
-
-$evidenceTable = new \Ease\Html\TableTag(null, ['class' => 'table']);
-$evidenceTable->addRowHeaderColumns();
-
-$evidencer = new \FlexiPeeHP\EvidenceList();
-
-$evidenceInfo = \FlexiPeeHP\EvidenceList::$evidences[$evidence];
-
-foreach ($evidenceInfo as $porperty => $propertyData) {
-    $evidenceTable->addRowColumns([$porperty, $propertyData]);
-}
-
-$myEvidenciesRaw = $evidencer->getColumnsFromFlexibee('*');
-$myEvidenceRaw   = null;
-foreach ($myEvidenciesRaw['evidences']['evidence'] as $evidenceUsedInfo) {
-    if ($evidenceUsedInfo['evidencePath'] == $evidence) {
-        $myEvidenceRaw = $evidenceUsedInfo;
+    $method = $oPage->getRequestValue('method');
+    $body   = $oPage->getRequestValue('body');
+    if (is_null($body)) {
+        $body = $evobj->jsonizeData([]);
     }
-}
 
-if (is_null($myEvidenceRaw)) {
-    $state = _('no');
-} else {
-    $state = _('yes');
-}
-$evidenceTable->addRowColumns([_('Allowed by license'), $state]);
+    $tabs->addTab(_('Query'),
+        new \Ease\TWB\Panel(_('User Query'), 'warning',
+        new ui\SendForm($url, $method, $body)));
 
-$infoRow = new \Ease\TWB\Row();
-$infoRow->addColumn(6, $evidenceTable);
+    $overviewUrl = $evobj->getEvidenceUrl().'/properties.html?inDesktopApp=true';
 
-$relationsList = new \Ease\Html\UlTag();
-
-foreach ($evobj->getRelationsInfo() as $relation) {
-    if (is_array($relation)) {
-        $relationsList->addItemSmart(' <a href="evidence.php?evidence='.$relation['url'].'">'.
-            $relation['name'].' ('.$relation['evidenceType'].')</a>');
+    $tabs->addTab(_('Items overview'),
+        new \Ease\Html\IframeTag($overviewUrl,
+        ['style' => 'width: 100%; height: 600px', 'frameborder' => 0]));
+    if (strstr($url, '?')) {
+        $overviewUrl = $url.'&inDesktopApp=true';
     } else {
-        $relationsList->addItemSmart($relation);
+        $overviewUrl = $url.'?inDesktopApp=true';
     }
+    $tabs->addTab(_('FlexiBee'),
+        new \Ease\Html\IframeTag(str_replace('.json', '.html', $overviewUrl),
+        ['style' => 'width: 100%; height: 600px', 'frameborder' => 0]));
+
+
+    $evidenceTable = new \Ease\Html\TableTag(null, ['class' => 'table']);
+    $evidenceTable->addRowHeaderColumns();
+
+    $evidencer = new \FlexiPeeHP\EvidenceList();
+
+    $evidenceInfo = \FlexiPeeHP\EvidenceList::$evidences[$evidence];
+
+    foreach ($evidenceInfo as $porperty => $propertyData) {
+        $evidenceTable->addRowColumns([$porperty, $propertyData]);
+    }
+
+    $myEvidenciesRaw = $evidencer->getColumnsFromFlexibee('*');
+    $myEvidenceRaw   = null;
+    foreach ($myEvidenciesRaw['evidences']['evidence'] as $evidenceUsedInfo) {
+        if ($evidenceUsedInfo['evidencePath'] == $evidence) {
+            $myEvidenceRaw = $evidenceUsedInfo;
+        }
+    }
+
+    if (is_null($myEvidenceRaw)) {
+        $state = _('no');
+    } else {
+        $state = _('yes');
+    }
+    $evidenceTable->addRowColumns([_('Allowed by license'), $state]);
+
+    $infoRow = new \Ease\TWB\Row();
+    $infoRow->addColumn(6, $evidenceTable);
+
+    $relationsList = new \Ease\Html\UlTag();
+
+    $evidenceNames = array_flip(\FlexiPeeHP\EvidenceList::$name);
+
+    foreach ($evobj->getRelationsInfo() as $relation) {
+        if (is_array($relation)) {
+            if (isset(\FlexiPeeHP\EvidenceList::$name[$relation['url']])) {
+                $relationsList->addItemSmart(' <a href="evidence.php?evidence='.$relation['url'].'">'.
+                    $relation['name'].' ('.$relation['evidenceType'].' <strong>'.$relation['url'].'</strong>)</a>');
+            } else {
+                if (array_key_exists($relation['name'], $evidenceNames)) {
+                    $relationsList->addItemSmart(' <a href="evidence.php?evidence='.$evidenceNames[$relation['name']].'"><strong>'.
+                        $relation['name'].'</strong> ('.$relation['evidenceType'].' '.$relation['url'].')</a>');
+                } else {
+                    if (array_key_exists(strtolower($relation['evidenceType']),
+                            \FlexiPeeHP\EvidenceList::$name)) {
+                        $relationsList->addItemSmart(' <a href="evidence.php?evidence='.strtolower($relation['evidenceType']).'">'.
+                            $relation['name'].' (<strong>'.$relation['evidenceType'].'</strong> '.$relation['url'].')</a>');
+                    } else {
+                        $relationsList->addItemSmart($relation['name'].' ('.$relation['evidenceType'].' '.$relation['url'].')</a>');
+                    }
+                }
+            }
+        } else {
+            $relationsList->addItemSmart($relation);
+        }
+    }
+
+    $infoRow->addColumn(6,
+        new \Ease\TWB\Panel(_('Relations'), 'info', $relationsList));
+
+    $tabs->addTab(_('Info'), $infoRow);
+
+
+
+
+    $oPage->container->addItem($tabs);
+
+    $oPage->addItem(new ui\PageBottom());
+
+    $oPage->draw();
 }
-
-$infoRow->addColumn(6,
-    new \Ease\TWB\Panel(_('Relations'), 'info', $relationsList));
-
-$tabs->addTab(_('Info'), $infoRow);
-
-
-
-
-$oPage->container->addItem($tabs);
-
-$oPage->addItem(new ui\PageBottom());
-
-$oPage->draw();
