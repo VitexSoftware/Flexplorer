ed96dc9c3d28ff43968d49f6550b072476737529
Vítězslav Dvořák <info@vitexsoftware.cz>
2017-07-03 09:21:49 +0200

    Codeception first test for login page

---
+++ tests/acceptance/LoginCept.php
@@ -0,0 +1,11 @@
+<?php 
+$I = new AcceptanceTester($scenario);
+$I->wantTo('perform actions and see result');
+$I->amOnPage('/Flexplorer/src/login.php');
+$I->seeElement("form input[name=\"server\"]");
+$I->seeElement("form input[name=\"login\"]");
+$I->seeElement("form input[name=\"password\"]");
+$I->seeElement("form button[class=\"btn btn-success\"]");
+$I->cantSee('Error');
+$I->cantSee('Warning');
+
